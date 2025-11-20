1b26095647d832222c286e61e6f265e93462c058
safayet nur <safayetnurelectra@gmail.com>
2025-11-20 22:59:51 +0600

    project initialization

---
+++ routes/postRoutes.js
@@ -0,0 +1,48 @@
+import express from "express";
+import {
+  addComment,
+  addRating,
+  createBlogPost,
+  deleteBlogPostController,
+  findRelatedPostsByCategory,
+  getAllBlogPostsController,
+  getBlogPostByIdController,
+  getBlogPostByUserId,
+  getCategoryIdWisePosts,
+  getCategoryWisePostsController,
+  incrementShareCount,
+  toggleReaction,
+  updateBlogPostController,
+  updatePostStatusController,
+} from "../controllers/postController.js";
+import { requireSignIn } from "../middlewares/authMiddleware.js";
+import { uploadImageController } from "../controllers/uploadImageController.js";
+
+const router = express.Router();
+
+// post a create blog
+router.post("/posts", requireSignIn, createBlogPost);
+// get blog by id
+router.get("/posts/:id", getBlogPostByIdController);
+// get blog by userid
+router.get("/posts/user/:userId", getBlogPostByUserId);
+
+// get blog
+router.get("/posts", getAllBlogPostsController);
+// delete blog by id
+router.delete("/posts/:id", deleteBlogPostController);
+// Update a blog post by ID
+router.put("/posts/:id", requireSignIn, updateBlogPostController);
+// image upload
+router.use("/upload-image", uploadImageController);
+router.patch("/posts/:id/status", updatePostStatusController);
+
+router.post("/posts/:postId/add-comments", addComment);
+router.post("/posts/:postId/add-rating", addRating);
+router.post("/posts/:postId/toggleReaction", toggleReaction);
+router.post("/posts/:postId/incrementShareCount", incrementShareCount);
+router.get("/category-wise-posts", getCategoryWisePostsController);
+router.get("/category-wise-posts/:categoryId", getCategoryIdWisePosts);
+router.get("/posts/related/:categoryId", findRelatedPostsByCategory);
+
+export default router;
---
+++ server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import tagRoutes from "./routes/tagRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import { v2 as cloudinary } from "cloudinary";
+dotenv.config();
+
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+//database
+connectDB();
+
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/tag", tagRoutes);
+app.use("/api/v1/post", postRoutes);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Dentists portal backend</h1>");
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} for dentists-portal mode on ${PORT}`
+      .bgRed.white
+  );
+});
